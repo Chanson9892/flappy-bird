af183ccd987218ace43239b4d40c40ccecb67c3a
Chanson9892 <chan.hanson5@gmail.com>
2021-03-15 17:39:00 -0700

    animation for bird works, misspelled content

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContendLoaded' , () => {
+document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground')
@@ -33,8 +33,25 @@ document.addEventListener('DOMContendLoaded' , () => {
 
 
     function generateObstacle() {
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
         obstacle.classList.add('obstacle')
         gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if(obstacleLeft === -60) {
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20)
     }
+    generateObstacle()
 })
